949f3a327fdb4c70aefbdf0fa337309d3dd25cb8
UAA <uaa04@yandex.ru>
2022-10-02 12:23:44 +0900

    добавил решение 4 задачи

---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int num1 = new Random().Next(-1000, 1000);
+int num2 = new Random().Next(-1000, 1000);
+int num3 = new Random().Next(-1000, 1000);
+Console.WriteLine("приняты числа: " + num1 +" "+ num2 +" "+ num3);
+int num_max = num1;
+
+if (num2 > num_max) num_max = num2;
+if (num3 > num_max) num_max = num3;
+
+Console.WriteLine("максимальное " + num_max);

9dd1fe12791488fec78f93f2a8e8b0caa885365f
UAA <uaa04@yandex.ru>
2022-10-02 12:46:49 +0900

    добавил решение 8 задачи

---
+++ lesson1/task8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("введите целое число ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("целые числа от 1 до "+ num.ToString() +":");
+for (int x=2; x <= num; x+=2) {
+    Console.Write(x.ToString() + ", ");
+}

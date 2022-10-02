ad2175b6ee535b729de1a2ac820c5a19a579eefb
UAA <uaa04@yandex.ru>
2022-10-02 22:50:47 +0900

    добавил занятия на 2 семинаре

---
+++ lesson2/task21/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа проверяет пятизначное число на палиндромом.
+
+int num = new Random().Next(10000, 100000);
+//int num = 12321;
+
+bool ch15(int num) {
+    return num / 10000 == num % 10;
+}
+bool ch24(int num) {
+    return num /1000 % 10 == num /10 % 10;
+}
+
+if (ch15(num) & ch24(num)) {
+    Console.WriteLine(num + " это полиндром");
+}
+else Console.WriteLine(num + " это НЕ полиндром");
---
+++ lesson2/task8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+string step(int num) {
+    string text = "";
+    for (int x=2; x <= num; x+=2) {
+        //Console.Write(x.ToString() + ", ");
+        text += $"{x} ";
+    }
+    return text;
+}
+void print(string text, int num) {
+    Console.WriteLine("целые числа от 1 до "+ num.ToString() +":");
+    Console.WriteLine(text);
+}
+Console.WriteLine("введите целое число ");
+int num = int.Parse(Console.ReadLine());
+print(step(num), num);
+

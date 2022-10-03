acf440a5eb117365222a6d549787eccbdc9d5a3e
UAA <uaa04@yandex.ru>
2022-10-03 22:07:52 +0900

    добавил решение 15 задачи

---
+++ lesson2/task15/Program.cs
@@ -0,0 +1,12 @@
+﻿
+bool check_day(int num) {
+    return num == 6 | num == 7;
+}
+
+Console.Write("цифру, обозначающую день недели (1-7) ");
+int number = int.Parse(Console.ReadLine());
+if (number < 1 | number > 7) Console.WriteLine("не правильный ввод");
+else {
+    if (check_day(number)) Console.WriteLine("выходной день");
+    else                   Console.WriteLine("рабочий день");
+}

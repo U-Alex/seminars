6f46d9971ae8f6504534cb3231d392dfaacf65e0
UAA <uaa04@yandex.ru>
2022-10-03 21:29:29 +0900

    добавил решение 10 задачи

---
+++ lesson2/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int second_digit(int num) {
+    return num / 10 % 10;
+}
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine("случайное число - " + num);
+int s_digit = second_digit(num);
+Console.WriteLine("вторая цифра - " + s_digit);

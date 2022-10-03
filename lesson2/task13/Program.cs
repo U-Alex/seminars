44aefe1a5566c5ea6a854e36a714e47c2a51254a
UAA <uaa04@yandex.ru>
2022-10-03 21:56:41 +0900

    добавил решение 13 задачи

---
+++ lesson2/task13/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int check_3_digit(int num) {
+    while (num > 999) num /= 10;
+    return num % 10;
+}
+Console.Write("введите целое число ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100) Console.WriteLine("третьей цифры нет");
+else {
+    int num = check_3_digit(number);
+    Console.WriteLine(num);
+}

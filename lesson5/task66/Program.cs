844d2fc41afb4c18a6c651a6b76d907578913598
UAA <uaa04@yandex.ru>
2022-10-15 19:37:50 +0900

    добавил решение 66 задачи

---
+++ lesson5/task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int inputNum() {
+    int num = 0;
+    bool ok = false;
+    while (!ok) {
+        Console.Write("введите целое число:  ");
+        ok = int.TryParse(Console.ReadLine(), out num);
+        if (ok && (num <= 0)) ok = false;
+    }
+    return num;
+}
+
+string addNum(int num_st, int num_end, string res="") {
+    if (num_st < num_end) {
+        res += num_st.ToString() + ",";
+        return addNum(++num_st, num_end, res);
+    }
+    return res += num_st.ToString();;
+}
+
+int num = inputNum();
+string result = addNum(1, num);
+Console.WriteLine($"натуральные числа от 1 до {num}: {result}");

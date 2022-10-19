ea75e8a87791b49bb5c67249aaa5f07565ae7103
UAA <uaa04@yandex.ru>
2022-10-19 22:22:02 +0900

    добавил решение 41 задачи

---
+++ lesson6/task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+List<int> inputNum() {
+    bool ok = false;
+    List<int> nums = new List<int>();
+    while (!ok) {
+        Console.WriteLine("введите целые числа через запятую или через пробел, например 3,5 -4 100");
+        string[] inp = Console.ReadLine().Replace(" ", ",").Trim(new Char[] {' ', ','}).Split(",");
+        int len = inp.Length;
+        if (len > 1) {
+            int temp;
+            nums.Clear();
+            for (int i=0; i < len; i++) {
+                if (int.TryParse(inp[i], out temp)) {
+                    ok = true;
+                    nums.Add(temp);
+                }
+                else {
+                    ok = false;
+                    Console.WriteLine("ошибка парсинга");
+                    break;
+                }
+            }
+        }
+    }
+    return nums;//(ok) ? nums : (-1);
+}
+
+int posNum(List<int> nums) {
+    int cnt = 0;
+    foreach (int num in nums) {
+        if (num > 0) cnt++;
+    }
+    return cnt;
+}
+
+List<int> nums = inputNum();
+int count = posNum(nums);
+
+Console.WriteLine($"количество чисел больше 0: {count} из {nums.Count}");
---
+++ lesson6/task53/Program.cs
@@ -0,0 +1,28 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+
+int[,] createArr(int a, int b) {
+    int[,] array = new int[a, b];
+    for (int i=0; i < array.GetLength(1); i++) {
+        for (int j=0; j < array.GetLength(0); j++) {
+            array[j, i] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+// int inputNum() {
+//     int a = 0;
+//     bool ok = false;
+//     while (!ok) {
+//         Console.WriteLine("введите число ");
+//         string num = Console.ReadLine();
+//             if (int.TryParse(num[0], out a) &&
+//                 int.TryParse(num[1], out b)) {
+//                     if (b > 0) {
+//                         ok = true;
+//                     }
+//                 }
+    
+//     }
+//     return new int[2]{a, b};
+// } 
+

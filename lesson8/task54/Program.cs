e5a25c95f9cd5b69ee49034f2f4b538aa5ea013f
UAA <uaa04@yandex.ru>
2022-10-24 21:41:03 +0900

    добавил решение 54 задачи

---
+++ lesson8/task54/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] CreateArray(int a, int b) {
+    int[,] array = new int[a, b];
+    for (int i=0; i < b; i++) {
+        for (int j=0; j < a; j++) {
+            array[j, i] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(ref int[,] array) {
+    int a = array.GetLength(0);
+    int b = array.GetLength(1);
+    string separator = new System.Text.StringBuilder().Insert(0, "---", a).ToString();
+    for (int i=0; i < b; i++) {
+        for (int j=0; j < a; j++) {
+            Console.Write($"{array[j, i]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine(separator);
+}
+void SortArray(ref int[,] array) {
+    int a = array.GetLength(0);
+    int b = array.GetLength(1);
+    int[] line = new int[a];
+    for (int i=0; i < b; i++) {
+        for (int j=0; j < a; j++) line[j] = array[j, i];
+        Array.Sort(line);
+        Array.Reverse(line);
+        for (int j=0; j < a; j++) array[j, i] = line[j] ;
+    }
+}
+
+int rows = 7;
+int columns = 4;
+Console.Clear();
+int[,] array = CreateArray(rows, columns);
+PrintArray(ref array);
+SortArray(ref array);
+PrintArray(ref array);
---
+++ lesson_/task76/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+List<int> CreateNumList(int st, int end) {
+    List<int> nums = new List<int>();
+    for (int i = st; i < end; i++) nums.Add(i);
+    return nums;
+}
+
+List<List<int>> GroupsNumbers(int num) {
+    List<List<int>> grps = new List<List<int>>();
+    grps.Add(new List<int>(){1});
+    List<int> numList = CreateNumList(2, num);
+    
+    while (numList.Count != 0) {
+        List<int> grp = numList.ToList();
+        for (int i = 0; i < grp.Count; i++) {
+            grp.RemoveAll(num => num != grp[i] && num % grp[i] == 0);
+        }
+        grps.Add(grp);
+        numList.RemoveAll(num => grp.Contains(num));
+    }
+    return grps;
+}
+
+List<List<int>> res = GroupsNumbers(50);
+
+foreach (List<int> ob in res) {
+    foreach (int ob2 in ob) {
+        Console.Write($"{ob2} ");
+    }
+    Console.WriteLine();
+}
+

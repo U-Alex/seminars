ae9626e68d6270f35f1e934fe300b51641e9700f
UAA <uaa04@yandex.ru>
2022-11-02 20:14:49 +0900

    fix

--- lesson8/task65/Program.cs
+++ lesson8/task65/Program.cs
@@ -1,10 +1,10 @@
 ﻿//Спирально заполнить двумерный массив
 
-void FillArray(ref int[,] mass, int body=1) {
-    int iLen = mass.GetLength(0);
-    int jLen = mass.GetLength(1);
-    int[] currIdx = new int[] {0,0};
-    int[] nextIdx = new int[] {0,0};
+void FillArray(ref int[,] arr, int body=1) {
+    int iLen = arr.GetLength(0);
+    int jLen = arr.GetLength(1);
+    int[] currIdx = new int[] {0, 0};
+    int[] nextIdx = new int[] {0, 0};
     int idx_rule = 0;
     int[][] rule = new int[][] {
         new int[] {1,0},
@@ -12,14 +12,14 @@ void FillArray(ref int[,] mass, int body=1) {
         new int[] {-1,0},
         new int[] {0,-1}
     };
-    for (int step = 0; step < iLen*jLen; step++) {
-        mass[currIdx[0],currIdx[1]] = body++;
+    for (int step = 0; step < iLen * jLen; step++) {
+        arr[currIdx[0],currIdx[1]] = body++;
         nextIdx[0] = currIdx[0] + rule[idx_rule][0];
         nextIdx[1] = currIdx[1] + rule[idx_rule][1];
         if ((nextIdx[0] < 0) || (nextIdx[0] >= iLen) ||
             (nextIdx[1] < 0) || (nextIdx[1] >= jLen) ||
-            (mass[nextIdx[0], nextIdx[1]] > 0)) {
-                idx_rule = (idx_rule > 2) ? 0 : idx_rule+1 ;
+            (arr[nextIdx[0], nextIdx[1]] > 0)) {
+                idx_rule = (idx_rule > 2) ? 0 : ++idx_rule ;
                 currIdx[0] += rule[idx_rule][0];
                 currIdx[1] += rule[idx_rule][1];
         }
@@ -35,7 +35,7 @@ void PrintArray(ref int[,] mass) {
     Console.Clear();
     for (int j=0; j < jLen; j++) {
         for (int i=0; i < iLen; i++) {
-            Console.Write($"{mass[i,j]}\t");
+            Console.Write($"{mass[i, j]}\t");
         }
         Console.WriteLine();
     }
